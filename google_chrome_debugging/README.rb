efc5b7dc367f9619df016dcd348be262152ef8ba
Anthony Severini <FluffyCode@gmail.com>
2012-10-13 13:51:15 -0700

    Update README.rb.

--- google_chrome_debugging/README.rb
+++ google_chrome_debugging/README.rb
@@ -1,4 +1,6 @@
 # Debugging in Google Chrome - CoderDojo session/project.
+# From:
+	# https://github.com/hcopperm/coderdojo_debugging/blob/master/debugging.md
 
 # Created Saturday, 10/13/2012
 # Additionally modified:
