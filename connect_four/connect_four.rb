b9e071ad5649271889e5d76b210055f203fe9b27
Anthony Severini <FluffyCode@gmail.com>
2012-10-27 13:33:07 -0700

    Backtrack - remove contents of animate-do's if.

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -24,19 +24,7 @@ Shoes.app :width => 900, :height => 625 do
 		over_y = y % 100
 
 		if button == 1
-			if (5..105).include?(over_x)
-				if (5..105).include?(over_y)
-					@turn += 1
-
-					if @turn % 2 == 1
-						color = red
-					else
-						color = black
-					end
-
-					@board[(7 * row) + column].style(:fill => color)
-				end
-			end
+			
 		end
 	end
 
