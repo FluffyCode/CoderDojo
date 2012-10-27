68666a216c61649486515007a11ea7fe3a7216bc
Anthony Severini <FluffyCode@gmail.com>
2012-10-27 11:47:45 -0700

    October 27th's CoderDojo session, headed by GitHubs's Brent Beer - initial commit.

---
+++ oct_27_2012/setup.rb
@@ -0,0 +1,10 @@
+Shotes.app :width => 900, :height => 625 do
+	background black
+	blue = "#0000FF"
+
+	# Build the grid
+	4.times do |column|
+		rect 200 * column + 5, 5, 100, 100, :fill => blue
+	end
+
+end
