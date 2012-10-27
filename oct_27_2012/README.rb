e435042d82ad28f60b59a554d140047eb903fdae
Anthony Severini <FluffyCode@gmail.com>
2012-10-27 11:58:33 -0700

    Update README.

--- oct_27_2012/README.rb
+++ oct_27_2012/README.rb
@@ -1,3 +1,4 @@
 # October 27th GitHub CoderDojo session - headed by Brent Beer:
 	# https://github.com/brntbeer
+	# https://github.com/brntbeer/lesson-plans/blob/connect-four/shoes/connect-four.md
 
