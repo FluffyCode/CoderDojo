8b86469a5a5058ff0d96283d293ca02622cbc5b4
Anthony Severini <FluffyCode@gmail.com>
2012-10-27 12:36:15 -0700

    Fix rect.

--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -8,7 +8,7 @@ Shoes.app :width => 900, :height => 625 do
 	6.times do |row|
 		7.times do |column|
 			r = rect 100 * column + 5,
-				100 * row + 5, 100, :fill => white
+				100 * row + 5, 100, 100, :fill => white
 			@board << r
 		end
 	end
