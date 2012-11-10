e7c7201efc7b2cfc6c2323ca77977c3026e1c6e3
Anthony Severini <FluffyCode@gmail.com>
2012-11-10 13:13:10 -0800

    Update game to latest version (does not yet properly declare a win).

--- connect_four/README.rb
+++ connect_four/README.rb
@@ -5,3 +5,5 @@
 	# https://github.com/brntbeer/lesson-plans/blob/connect-four/shoes/connect-four.md
 
 # Created Saturday, 10/27/2012
+	# Saturday, 11/10/2012
+		# Finalized version for CoderDojo.
--- connect_four/connect_four.rb
+++ connect_four/connect_four.rb
@@ -6,6 +6,7 @@ Shoes.app :width => 900, :height => 625 do
 	@turn = 0
 	@board = []
 	@picked = []
+	@columns = [ [], [], [], [], [], [], [] ]
 
 	# Build the grid
 	6.times do |row|
@@ -13,6 +14,7 @@ Shoes.app :width => 900, :height => 625 do
 			r = rect 100 * column + 5,
 				100 * row + 5, 100, 100, :fill => white
 			@board << r
+			@columns[column] << row
 		end
 	end
 
@@ -23,6 +25,7 @@ Shoes.app :width => 900, :height => 625 do
 		row = y / 100
 
 		if button == 1
+			space = (@columns[column].last * 7) + column
 			if !@picked.include?(7 * row + column)
 				@turn += 1
 
@@ -32,9 +35,64 @@ Shoes.app :width => 900, :height => 625 do
 					color = black
 				end
 
-				@board[(7 * row) + column].style(:fill => color)
-				@picked << (7 * row + column)
+				@board[space].style(:fill => color)
+				@picked << space
+				@columns[column].pop
+
+				if solved?(color)
+					alert("#{color} wins!")
+				end
 			end
+		end
+	end
+
+	def vertical_win(color)
+		7.times do |column|
+			current_column = [0, 7, 14, 21, 28, 35].collect { |num| @board[column + num] }
+			current_column = current_column.drop_while { |rect| rect.style[:fill] != color }
+			current_column = current_column.take_while { |rect| rect.style[:fill] == color }
+
+			if current_column.count >= 4
+				return true
+			end
+		end
+		false
+	end
+
+	def horizontal_win(color)
+		6.times do |row|
+			current_row = @board[(row * 7)..(row * 7 + 6)]
+			continuous_row_count = 0
+			position = 0
+
+			while (position < current_row.length && continuous_row_count != 4) do
+				if (current_row[position].style[:fill] == color)
+					continuous_row_count += 1
+				else
+					continuous_row_count = 0
+				end
+				position += 1
+			end
+
+			if continuous_row_count >= 4
+				return true
+			end
+		end
+		false
+	end
+
+	# To be done later - solve for diagonals
+	def diagonal_win(color)
+		6.times do
+
+		end
+	end
+
+	def solved?(color)
+		if vertical_win(color) || horizontal_win(color)
+			return true
+		else
+			return false
 		end
 	end
 
